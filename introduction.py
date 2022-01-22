b235986031205ac45db8939fb95f0ea465597ee9
Alex Day <adday@clemson.edu>
2022-01-22 16:51:34 -0500

    Initial commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+
+if name.lower() == "alex":
+    print("Hi!")
+else:
+    for _ in range(5):
+        print("GO AWAY I DON'T KNOW YOU")
---
+++ walrus.py
@@ -0,0 +1,12 @@
+# Simple Assignment
+num = 15
+
+print(num)
+>>> 15
+
+print(num = 15)
+>>> TypeError
+
+# Walrus
+print(num := 15)
+>>> 15
